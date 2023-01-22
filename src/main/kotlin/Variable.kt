5ecdaf06ba61c086b3184209c34f177eb071b320
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-01-22 11:16:18 +0700

    Variable pada Kotlin

---
+++ src/main/kotlin/Variable.kt
@@ -0,0 +1,8 @@
+fun main() {
+    // Mutable variable
+    var age: Int = 19;
+    var nickName = "tamathecxder"; // not explicit but it will autodetect
+
+    // Imutable variable
+    val API_KEY = "5e450478-9a0b-11ed-a8fc-0242ac120002";
+}
