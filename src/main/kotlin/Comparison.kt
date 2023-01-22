60e61398b93c6deb44bace0b4b5bc325b9ee673d
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-01-22 17:14:18 +0700

    Operasi Perbandingan pada Kotlin

---
+++ src/main/kotlin/Comparison.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val x: Int = 50;
+    val y: Int = 50;
+
+    val result1: Boolean = x > y;
+    val result2: Boolean = x == y;
+    val result3: Boolean = x >= y;
+
+    println(result1);
+    println(result2);
+    println(result3);
+}
