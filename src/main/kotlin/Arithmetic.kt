bc2544493eb7638586ac638e26d0ee3cefb14d44
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-01-22 17:09:08 +0700

    Operasi Aritmatika pada Kotlin

---
+++ src/main/kotlin/Arithmetic.kt
@@ -0,0 +1,25 @@
+fun main() {
+    var value1: Int = 10;
+    var value2: Int = 20;
+
+    // Math operator
+    val tambah: Int = value1 + value2;
+    val kurang: Int = value1 + value2;
+    val kali: Int = value1 * value2;
+    val bagi: Int = value1 / value2;
+
+    println(tambah);
+    println(kurang);
+    println(bagi);
+    println(kali);
+
+    // Augmented Operator
+    var total: Int = 0;
+    val value3: Int = 5;
+    val value4: Int = 8;
+
+    total += value3;
+    total *= value4;
+
+    println(total);
+}
