968c92712ab1c6f04d55c4281f7471bc49449242
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-01-22 11:32:28 +0700

    Constant pada Kotlin

--- src/main/kotlin/Variable.kt
+++ src/main/kotlin/Variable.kt
@@ -1,8 +1,18 @@
+const val API_KEY = "5e450478-9a0b-11ed-a8fc-0242ac120002";
+const val APP_VERSION = "1.0.0";
+
 fun main() {
+
     // Mutable variable
     var age: Int = 19;
     var nickName = "tamathecxder"; // not explicit but it will autodetect
 
     // Imutable variable
-    val API_KEY = "5e450478-9a0b-11ed-a8fc-0242ac120002";
+    val users_id = "afw82kw2d213";
+
+    // Nullable variable
+    var datetime = null;
+    var birthDate: String?;
+
+    println("My API KEY: $API_KEY | $APP_VERSION");
 }
