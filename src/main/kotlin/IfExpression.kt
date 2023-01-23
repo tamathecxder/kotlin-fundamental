64f66d9b780f6f0c049c3fdd683151d7b201a7ad
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-01-23 09:31:24 +0700

    If, If-Else, Else If, Operation pada Kotlin

---
+++ src/main/kotlin/IfExpression.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val nilai: Int = 72;
+
+    if (nilai >= 85) {
+        println("Selamat, nilai anda diatas rata-rata");
+    } else if (nilai >= 75) {
+        println("Nilai anda pas");
+    } else {
+        println("Harap lakukan remedial!");
+    }
+}
