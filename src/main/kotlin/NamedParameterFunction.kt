7e5d7aa2d882c791b2553d7b8c70c6bc6dcfd32d
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-01-23 13:32:37 +0700

    Named Parameter Function pada Kotlin

---
+++ src/main/kotlin/NamedParameterFunction.kt
@@ -0,0 +1,11 @@
+fun bertegurSapa(nama: String, waktu: String, sapaan: String) {
+    println("Selamat $waktu, $sapaan $nama");
+}
+
+fun main() {
+    bertegurSapa(
+        nama = "Eko",
+        sapaan = "Tuan",
+        waktu = "Pagi",
+    );
+}
